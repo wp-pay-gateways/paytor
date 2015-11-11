dde5484d0129764e609697c5565cfc1a4d354342
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 16:33:02 +0100

    Simplified the gateway integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,8 +1,12 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Paytor_GatewayIntegration {
+class Pronamic_WP_Pay_Gateways_Paytor_Integration {
 	public function __construct() {
-		$this->id = 'paytor';
+		$this->id            = 'paytor';
+		$this->name          = 'Paytor';
+		$this->url           = 'http://paytor.com/';
+		$this->dashboard_url = 'http://paytor.com/';
+		$this->provider      = 'paytor';
 	}
 
 	public function get_config_factory_class() {
