be0a3dd1415736f887dd2b7aba68a55712314eb0
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:08:58 +0100

    Extend from Mollie library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,23 +1,11 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Paytor_Integration {
+class Pronamic_WP_Pay_Gateways_Paytor_Integration extends Pronamic_WP_Pay_Gateways_Mollie_Integration {
 	public function __construct() {
 		$this->id            = 'paytor';
 		$this->name          = 'Paytor';
 		$this->url           = 'http://paytor.com/';
 		$this->dashboard_url = 'http://paytor.com/';
 		$this->provider      = 'paytor';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Mollie_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Mollie_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Mollie_Gateway';
 	}
 }
