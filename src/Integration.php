25b7eeb7cacaad9d4b7cca9f69de33a0a55dcb7e
Remco Tolsma <info@remcotolsma.nl>
2016-01-04 11:35:17 +0100

    Added tests.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: Paytor - Integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Paytor_Integration extends Pronamic_WP_Pay_Gateways_Mollie_Integration {
 	public function __construct() {
 		$this->id            = 'paytor';
